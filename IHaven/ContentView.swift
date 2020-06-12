def1840047eb65fd9e928a22830deed12de0cefe
Laxse <laxse@outlook.com>
2020-06-12 22:48:50 +0800

    Initial Commit

---
+++ IHaven/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  IHaven
+//
+//  Created by 梁霄 on 2020/6/12.
+//  Copyright © 2020 梁霄. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
