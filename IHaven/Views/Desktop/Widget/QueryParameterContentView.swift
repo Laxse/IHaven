a3871d4e8a7207505d57a1eab103a79828993b9a
Laxse <laxse@outlook.com>
2021-09-17 00:44:42 +0800

    single view panel

--- IHaven/Views/Desktop/Widget/QueryParameterContentView.swift
+++ IHaven/Views/Desktop/Widget/QueryParameterContentView.swift
@@ -49,7 +49,7 @@ struct QueryParameterContentView: View {
                 .padding(.horizontal, 20)
                 .padding(.top, 0)
                 .padding(.bottom, 20)
-            }.edgesIgnoringSafeArea(.all)
+            }.edgesIgnoringSafeArea(.all).blur(radius: 3.0)
             HStack(alignment: .center) {
                 Spacer()
                 Pagination(count: 5000, size: 24, active: self.imageRepository.query.page,changeFunc: { page in
@@ -64,14 +64,57 @@ struct QueryParameterContentView: View {
             }.edgesIgnoringSafeArea(.all)
            
             HStack(alignment: .center, content: {
-                Spacer()
+                VStack(alignment: .leading, spacing: 0,content: {
+                    Text("1920 x 1080")
+                        .font(.title)
+                        .fontWeight(.ultraLight)
+                        .padding(.vertical, 20.0)
+                        .frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    HStack(alignment: .center, spacing: 0.0, content: {
+                        Rectangle().fill(Color.red)
+                            .frame(width: 30, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Rectangle().fill(Color.blue)
+                            .frame(width: 30, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Rectangle().fill(Color.gray)
+                            .frame(width: 30, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Rectangle().fill(Color.pink)
+                            .frame(width: 30, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                        Rectangle().fill(Color.purple)
+                            .frame(width: 30, height: 10, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    }).frame(width: 200, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                    Divider()
+                    Text("TAGS")
+                        .font(.callout)
+                        .fontWeight(.ultraLight)
+                        .multilineTextAlignment(.leading)
+                        .padding(.top,20)
+                        .padding(.leading, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        .padding(.bottom,10)
+                    Divider()
+                    Text("PROPERTIES")
+                        .font(.callout)
+                        .fontWeight(.ultraLight)
+                        .multilineTextAlignment(.leading)
+                        .padding(.top,20)
+                        .padding(.leading, /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                        .padding(.bottom,10)
+                    
+                    Spacer()
+                    Text("All images remain property of their original owners.")
+                        .fontWeight(.light)
+                        .foregroundColor(Color.gray)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                        .frame(width: 200)
+                        .border(Color.gray)
+                    Spacer()
+                    // tags
+                    // user info
+                })
+                .frame(minWidth: 200,idealWidth: 200,maxWidth: 200,maxHeight: .infinity, alignment: .center)
+                .background(Color.black)
                 VStack(alignment: .center, content: {
                     Spacer()
-//                    WebImage(url:URL(string: "https://w.wallhaven.cc/full/g7/wallhaven-g75r7d.jpg") )
-//                        .resizable()
-//                        .scaledToFill()
-//                        .clipped()
-//                        .cornerRadius(2.0)
                     Label(
                         title: { Text("Label") },
                         icon: { Image(systemName: "42.circle") })
@@ -79,9 +122,10 @@ struct QueryParameterContentView: View {
                 })
                 Spacer()
             })
-            .background(Color.red)
-            .padding(.horizontal, 50)
-            .padding(.vertical, 100)
+            .background(Color.black)
+            .opacity(0.9)
+            
+            
             .edgesIgnoringSafeArea(.all)
         }).frame(maxWidth: .infinity, maxHeight: .infinity)
     }
